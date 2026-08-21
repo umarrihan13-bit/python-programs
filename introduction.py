83095df3860a377b0585479073ec72af14349f1d
umarrihan13-bit <umar.rihan13@gmail.com>
2026-08-21 21:49:30 +0530

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+print("my name is Umar")
+
+print("i like playing  football")
+
+print("The result of 5 + 3 is",5+3)
+print("welcome to the world of programming!")
+print(4)
+print("Hello Python \n")
+
+print("keyword in python are.....")
+import keyword
+print(keyword.kwlist)
